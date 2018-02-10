6e5d2d3fe7adf17e4947a684d7e2b94c85be2c44
soywiz <soywiz@gmail.com>
2018-02-10 01:10:08 +0100

    Forgot some files

---
+++ ktor-client-cio-redis/src/main/kotlin/com/soywiz/io/ktor/client/redis/Redis.kt
@@ -0,0 +1,249 @@
+package com.soywiz.io.ktor.client.redis
+
+import com.soywiz.io.ktor.client.util.*
+import kotlinx.coroutines.experimental.delay
+import kotlinx.coroutines.experimental.runBlocking
+import java.io.IOException
+import java.nio.charset.Charset
+import java.util.concurrent.atomic.AtomicLong
+
+//private const val DEBUG = true
+private const val DEBUG = false
+
+// https://redis.io/topics/protocol
+class Redis(val maxConnections: Int = 50, val stats: Stats = Stats(), private val clientFactory: suspend () -> Client) :
+    RedisCommand {
+    companion object {
+        //suspend operator fun invoke(
+        operator fun invoke(
+            hosts: List<String> = listOf("127.0.0.1:6379"),
+            maxConnections: Int = 50,
+            charset: Charset = Charsets.UTF_8,
+            password: String? = null,
+            stats: Stats = Stats(),
+            bufferSize: Int = 0x1000
+        ): Redis {
+            val hostsWithPorts = hosts.map { HostWithPort.parse(it, 6379) }
+
+            var index: Int = 0
+
+            return Redis(maxConnections, stats) {
+                val tcpClient = AsyncClient(bufferSize)
+                //Console.log("tcpClient: ${tcpClient::class}")
+                Client(
+                    reader = tcpClient,
+                    reconnect = { client ->
+                        index = (index + 1) % hostsWithPorts.size
+                        val host = hostsWithPorts[index] // Round Robin
+                        tcpClient.connect(host.host, host.port)
+                        if (password != null) client.auth(password)
+                    },
+                    writer = tcpClient,
+                    closeable = tcpClient,
+                    charset = charset,
+                    stats = stats
+                ).apply {
+                    //init()
+                }
+            }
+        }
+
+        //private const val CR = '\r'.toByte()
+        private const val LF = '\n'.toByte()
+    }
+
+    class Stats {
+        val commandsQueued = AtomicLong()
+        val commandsStarted = AtomicLong()
+        val commandsPreWritten = AtomicLong()
+        val commandsWritten = AtomicLong()
+        val commandsErrored = AtomicLong()
+        val commandsFinished = AtomicLong()
+
+        override fun toString(): String {
+            return "Stats(commandsQueued=$commandsQueued, commandsStarted=$commandsStarted, commandsPreWritten=$commandsPreWritten, commandsWritten=$commandsWritten, commandsErrored=$commandsErrored, commandsFinished=$commandsFinished)"
+        }
+    }
+
+    class Client(
+        private val reader: AsyncInputStream,
+        private val writer: AsyncOutputStream,
+        private val closeable: AsyncCloseable,
+        private val charset: Charset = Charsets.UTF_8,
+        private val stats: Stats = Stats(),
+        private val reconnect: suspend (Client) -> Unit = {}
+    ) : RedisCommand {
+        suspend fun close() = this.closeable.close()
+
+        private val initOnce = Once()
+        private val commandQueue = AsyncQueue()
+
+        private suspend fun initOnce() {
+            initOnce {
+                commandQueue {
+                    try {
+                        reconnect(this@Client)
+                    } catch (e: IOException) {
+                    }
+                }
+            }
+        }
+
+        private suspend fun readValue(): Any? {
+            val line = reader.readUntil(LF).toString(charset).trim()
+            debug { "Redis[RECV]: $line" }
+            //val line = reader.readLine(charset = charset).trim()
+            //println(line)
+
+            if (line.isEmpty()) throw ResponseException("Empty value")
+            return when (line[0]) {
+                '+' -> line.substring(1) // Status reply
+                '-' -> throw ResponseException(line.substring(1)) // Error reply
+                ':' -> line.substring(1).toLong() // Integer reply
+                '$' -> { // Bulk replies
+                    val bytesToRead = line.substring(1).toInt()
+                    if (bytesToRead == -1) {
+                        null
+                    } else {
+                        val data = reader.readBytesExact(bytesToRead)
+                        reader.skip(2) // CR LF
+                        data.toString(charset).apply {
+                            debug { "Redis[RECV][data]: $this" }
+                        }
+                    }
+                }
+                '*' -> { // Array reply
+                    val arraySize = line.substring(1).toLong()
+                    (0 until arraySize).map { readValue() }
+                }
+                else -> throw ResponseException("Unknown param type '${line[0]}'")
+            }
+        }
+
+        val maxRetries = 10
+
+        private inline fun debug(msg: () -> String) {
+            if (DEBUG) println(msg())
+        }
+
+        override suspend fun commandAny(vararg args: Any?): Any? {
+            initOnce()
+            //println(args.toList())
+            stats.commandsQueued.incrementAndGet()
+            return commandQueue {
+                val cmd = StringBuilder()
+                cmd.append('*')
+                cmd.append(args.size)
+                cmd.append("\r\n")
+                for (arg in args) {
+                    //val sarg = "$arg".redisQuoteIfRequired()
+                    val sarg = "$arg"
+                    // Length of the argument.
+                    val size = sarg.toByteArray(charset).size
+                    cmd.append('$')
+                    cmd.append(size)
+                    cmd.append("\r\n")
+                    cmd.append(sarg)
+                    cmd.append("\r\n")
+                }
+
+                // Common queue is not required align reading because Redis support pipelining : https://redis.io/topics/pipelining
+                val dataString = cmd.toString()
+                val data = dataString.toByteArray(charset)
+                var retryCount = 0
+
+                debug { "Redis[SEND]: $dataString" }
+
+                retry@ while (true) {
+                    stats.commandsStarted.incrementAndGet()
+                    try {
+                        stats.commandsPreWritten.incrementAndGet()
+                        //Console.log("writer: $writer")
+                        //Console.log("writer: ${writer::class}")
+                        //Console.log(writer)
+                        //println("[a]")
+                        writer.writeFully(data) // @TODO: Maybe use writeAvailable instead of appending?
+                        writer.flush()
+                        //println("[b]")
+                        stats.commandsWritten.incrementAndGet()
+                        //println("[c]")
+                        val res = readValue()
+                        //println("[d]")
+                        stats.commandsFinished.incrementAndGet()
+                        return@commandQueue res
+                    } catch (t: IOException) {
+                        t.printStackTrace()
+                        stats.commandsErrored.incrementAndGet()
+                        try {
+                            reconnect(this@Client)
+                        } catch (e: Throwable) {
+                        }
+                        delay(500 * retryCount)
+                        retryCount++
+                        if (retryCount < maxRetries) {
+                            continue@retry
+                        } else {
+                            throw RuntimeException("Giving up with this redis request max retries $maxRetries")
+                        }
+                    } catch (t: Throwable) {
+                        stats.commandsErrored.incrementAndGet()
+                        println(t)
+                        throw t
+                    }
+                }
+            }
+        }
+    }
+
+    private val clientPool = AsyncPool(maxItems = maxConnections) { clientFactory() }
+
+    override suspend fun commandAny(vararg args: Any?): Any? = clientPool.tempAlloc { it.commandAny(*args) }
+
+    class ResponseException(message: String) : Exception(message)
+}
+
+interface RedisCommand {
+    suspend fun commandAny(vararg args: Any?): Any?
+}
+
+@Suppress("UNCHECKED_CAST")
+suspend fun RedisCommand.commandArray(vararg args: Any?): List<String> =
+    (commandAny(*args) as List<String>?) ?: listOf()
+
+suspend fun RedisCommand.commandString(vararg args: Any?): String? = commandAny(*args)?.toString()
+suspend fun RedisCommand.commandLong(vararg args: Any?): Long = commandAny(*args)?.toString()?.toLongOrNull() ?: 0L
+suspend fun RedisCommand.commandUnit(vararg args: Any?): Unit = run { commandAny(*args) }
+
+internal object RedisExperiment {
+    @JvmStatic
+    fun main(args: Array<String>): Unit {
+        runBlocking {
+            val redis = Redis(bufferSize = 1)
+            //val redis = Redis(bufferSize = 3)
+            redis.set("a", "hello")
+            println(redis.get("a"))
+        }
+    }
+}
+
+// @TODO: SLOWER:
+//val cmd = ByteArrayOutputStream()
+//val ps = PrintStream(cmd, true, Charsets.UTF_8.name())
+//
+//ps.print('*')
+//ps.print(args.size)
+//ps.print("\r\n")
+//for (arg in args) {
+//	val data = "$arg".toByteArray(charset)
+//	ps.print('$')
+//	ps.print(data.size)
+//	ps.print("\r\n")
+//	ps.write(data)
+//	ps.print("\r\n")
+//}
+//
+//// Common queue is not required align reading because Redis support pipelining : https://redis.io/topics/pipelining
+//return commandQueue {
+//	writer.writeBytes(cmd.toByteArray())
+//	readValue()
+//}
---
+++ ktor-client-cio-util/src/main/kotlin/com/soywiz/io/ktor/client/util/ClientUtil.kt
@@ -0,0 +1,476 @@
+package com.soywiz.io.ktor.client.util
+
+import java.io.*
+import java.net.InetSocketAddress
+import java.nio.ByteBuffer
+import java.nio.channels.AsynchronousSocketChannel
+import java.nio.channels.CompletionHandler
+import java.util.*
+import java.util.concurrent.*
+import java.util.concurrent.atomic.AtomicBoolean
+import java.util.concurrent.atomic.AtomicInteger
+import kotlin.coroutines.experimental.Continuation
+import kotlin.coroutines.experimental.CoroutineContext
+import kotlin.coroutines.experimental.startCoroutine
+import kotlin.coroutines.experimental.suspendCoroutine
+
+class Once {
+    var completed = false
+
+    inline operator fun invoke(callback: () -> Unit) {
+        if (!completed) {
+            completed = true
+            callback()
+        }
+    }
+}
+
+interface AsyncCloseable {
+    suspend fun close(): Unit
+}
+
+interface AsyncInputStream {
+    //suspend fun readBytesUpTo(count: Int): ByteArray
+    suspend fun read(): Int
+
+    suspend fun skip(count: Int): Unit
+    suspend fun readUntil(b: Byte): ByteArray
+    suspend fun readBytesUpTo(buffer: ByteArray, offset: Int = 0, size: Int = buffer.size - offset): Int
+}
+
+fun AsyncInputStream.toBuffered(bufferSize: Int) = this // @TODO
+
+interface AsyncOutputStream {
+    suspend fun writeFully(buffer: ByteArray, offset: Int = 0, size: Int = buffer.size - offset): Unit
+    suspend fun flush(): Unit
+}
+
+class AsyncClient(val bufferSize: Int = 0x1000) : AsyncInputStream,
+    AsyncOutputStream, AsyncCloseable {
+    private var sc = AsynchronousSocketChannel.open()
+    private val cb = CircularByteArray(32 - Integer.numberOfLeadingZeros(bufferSize))
+    private val buffer = ByteBuffer.allocate(cb.writeAvailable)
+
+    suspend fun connect(host: String, port: Int) = suspendCoroutine<Unit> { c ->
+        //sc.close()
+        sc.connect(InetSocketAddress(host, port), this, object : CompletionHandler<Void, AsyncClient> {
+            override fun completed(result: Void?, attachment: AsyncClient) = c.resume(Unit)
+            override fun failed(exc: Throwable, attachment: AsyncClient) = c.resumeWithException(exc)
+        })
+    }
+
+    override suspend fun close() {
+        sc.close()
+    }
+
+    override suspend fun read(): Int {
+        fillBufferIfRequired()
+        return if (cb.readAvailable > 0) (cb.get().toInt() and 0xFF) else -1
+    }
+
+    override suspend fun readUntil(b: Byte): ByteArray {
+        val out = ByteArrayOutputStream()
+        while (true) {
+            if (cb.readAvailable < 1) {
+                fillBufferIfRequired()
+                if (cb.readAvailable < 1) break // end of stream
+            }
+            val c = cb.get()
+            if (c == b) break
+            out.write(c.toInt())
+        }
+        return out.toByteArray()
+    }
+
+    override suspend fun skip(count: Int): Unit {
+        var pending = count
+        while (pending > 0) {
+            fillBufferIfRequired(pending)
+            val chunk = Math.min(pending, cb.readAvailable)
+            cb.skip(chunk)
+            pending -= chunk
+        }
+        //readBytesExact(count)
+    }
+
+    override suspend fun readBytesUpTo(buffer: ByteArray, offset: Int, size: Int): Int {
+        fillBufferIfRequired()
+        val read = Math.min(cb.readAvailable, size)
+        this.cb.get(buffer, offset, read)
+        return read
+    }
+
+    suspend private fun fillBufferIfRequired(required: Int = 1) {
+        if (cb.readAvailable < required) {
+            buffer.clear()
+            buffer.limit(cb.writeAvailable)
+            return suspendCoroutine { c ->
+                sc.read(buffer, this, object : CompletionHandler<Int, AsyncClient> {
+                    override fun completed(result: Int, attachment: AsyncClient) =
+                        c.resume(cb.put(buffer.apply { flip() }, result))
+
+                    override fun failed(exc: Throwable, attachment: AsyncClient) = c.resumeWithException(exc)
+                })
+            }
+        }
+    }
+
+    override suspend fun writeFully(buffer: ByteArray, offset: Int, size: Int): Unit = suspendCoroutine { c ->
+        sc.write(ByteBuffer.wrap(buffer, offset, size), this, object : CompletionHandler<Int, AsyncClient> {
+            override fun completed(result: Int, attachment: AsyncClient) = c.resume(Unit)
+            override fun failed(exc: Throwable, attachment: AsyncClient) = c.resumeWithException(exc)
+        })
+    }
+
+    override suspend fun flush() = Unit // Already flushed
+}
+
+suspend fun AsyncInputStream.readBytesExact(count: Int): ByteArray {
+    val out = ByteArray(count)
+    var offset = 0
+    var pending = out.size
+    while (pending > 0) {
+        val read = readBytesUpTo(out, offset, pending)
+        if (read <= 0) break
+        pending -= read
+        offset += read
+    }
+    if (pending > 0) throw IOException("Couldn't read $count but just $offset")
+    return out
+}
+
+class CircularByteArray(private val capacityBits: Int) {
+    init {
+        if (capacityBits !in 1 until 30) throw IOException("Invalid capacityBits $capacityBits")
+    }
+
+    private val capacity = (1 shl (capacityBits - 1))
+    private val mask = capacity - 1
+    private val data = ByteArray(capacity)
+    private var readPos: Int = 0
+    private var writePos: Int = 0
+    var readAvailable: Int = 0; private set
+    val writeAvailable: Int get() = capacity - readAvailable
+
+    fun skip(count: Int) {
+        if (count > readAvailable) throw IOException("Buffer is empty")
+        readPos = (readPos + count) and mask
+        readAvailable -= count
+    }
+
+    fun get(): Byte {
+        if (readAvailable < 1) throw IOException("Buffer is empty")
+        return data[readPos].apply {
+            readPos = (readPos + 1) and mask
+            readAvailable -= 1
+        }
+    }
+
+    fun get(v: ByteArray, offset: Int = 0, size: Int = v.size - offset) {
+        if (size > readAvailable) throw IOException("Buffer is empty")
+        // @TODO: Up to two arraycopy
+        for (n in 0 until size) {
+            v[offset + n] = data[readPos]
+            readPos = (readPos + 1) and mask
+        }
+        readAvailable -= size
+    }
+
+    fun put(v: Byte) {
+        if (writeAvailable < 1) throw IOException("Buffer is full")
+        data[writePos] = v
+        writePos = (writePos + 1) and mask
+        readAvailable += 1
+    }
+
+    fun put(v: ByteArray, offset: Int = 0, size: Int = v.size - offset) {
+        if (size > writeAvailable) throw IOException("Buffer is full")
+        // @TODO: Up to two arraycopy
+        for (n in 0 until size) {
+            data[writePos] = v[offset + n]
+            writePos = (writePos + 1) and mask
+        }
+        readAvailable += size
+    }
+
+    fun put(v: ByteBuffer, size: Int) {
+        if (size > writeAvailable) throw IOException("Buffer is full")
+        // @TODO: Up to two get
+        for (n in 0 until size) {
+            data[writePos] = v.get()
+            writePos = (writePos + 1) and mask
+        }
+        readAvailable += size
+    }
+}
+
+class Deferred<T> {
+    var resolved = false
+    var value: T? = null
+    var exception: Throwable? = null
+    private var continuations = ConcurrentLinkedQueue<Continuation<T>>()
+
+    fun resolve(value: T) {
+        this.value = value
+        this.resolved = true
+        flush()
+    }
+
+    fun reject(exception: Throwable) {
+        this.exception = exception
+        this.resolved = true
+        flush()
+    }
+
+    suspend fun await(): T = suspendCoroutine { c ->
+        continuations.add(c)
+        flush()
+    }
+
+    private fun flush() {
+        if (resolved) {
+            while (continuations.isNotEmpty()) {
+                val c = continuations.remove() ?: break
+                if (exception != null) {
+                    c.resumeWithException(exception!!)
+                } else {
+                    c.resume(value as T)
+                }
+            }
+        }
+    }
+}
+
+class AsyncQueue {
+    private var running = AtomicBoolean(false)
+    private var queue = ConcurrentLinkedQueue<suspend () -> Unit>()
+
+    suspend operator fun <T> invoke(func: suspend () -> T): T {
+        val deferred = Deferred<T>()
+
+        queue.add {
+            val result = try {
+                func()
+            } catch (e: Throwable) {
+                return@add deferred.reject(e)
+            }
+            deferred.resolve(result)
+        }
+        if (running.compareAndSet(false, true)) {
+            runTasks(getCoroutineContext())
+        }
+        return deferred.await()
+    }
+
+    private fun runTasks(baseContext: CoroutineContext) {
+        if (queue.isNotEmpty()) {
+            val item = queue.remove()
+            item.startCoroutine(object : Continuation<Unit> {
+                override val context: CoroutineContext = baseContext
+                override fun resume(value: Unit) = runTasks(baseContext)
+                override fun resumeWithException(exception: Throwable) = runTasks(baseContext)
+            })
+        } else {
+            running.set(false)
+        }
+    }
+}
+
+class AsyncPool<T>(val maxItems: Int = Int.MAX_VALUE, val create: suspend () -> T) {
+    var createdItems = AtomicInteger()
+    private val freedItem = LinkedList<T>()
+
+    suspend fun <TR> tempAlloc(callback: suspend (T) -> TR): TR {
+        val item = alloc()
+        try {
+            return callback(item)
+        } finally {
+            free(item)
+        }
+    }
+
+    suspend fun alloc(): T {
+        return if (createdItems.get() >= maxItems) {
+            freedItem.remove()!!
+        } else {
+            createdItems.addAndGet(1)
+            create()
+        }
+    }
+
+    fun free(item: T) {
+        freedItem.add(item)
+    }
+}
+
+data class HostWithPort(val host: String, val port: Int) {
+    companion object {
+        fun parse(str: String, defaultPort: Int): HostWithPort {
+            val parts = str.split(':', limit = 2)
+            return HostWithPort(parts[0], parts.getOrElse(1) { "$defaultPort" }.toInt())
+        }
+    }
+}
+
+suspend fun getCoroutineContext(): CoroutineContext = suspendCoroutine<CoroutineContext> { c ->
+    c.resume(c.context)
+}
+
+object Hex {
+    val DIGITS = "0123456789ABCDEF"
+    val DIGITS_UPPER = DIGITS.toUpperCase()
+    val DIGITS_LOWER = DIGITS.toLowerCase()
+
+    fun isHexDigit(c: Char) = c in '0'..'9' || c in 'a'..'f' || c in 'A'..'F'
+
+    fun decode(str: String): ByteArray {
+        val out = ByteArray(str.length / 2)
+        for (n in 0 until out.size) {
+            val n2 = n * 2
+            out[n] = (str.substring(n2, n2 + 2).toIntOrNull(16) ?: 0).toByte()
+        }
+        return out
+    }
+
+    fun encode(src: ByteArray): String =
+        encodeBase(src, DIGITS_LOWER)
+
+    fun encodeLower(src: ByteArray): String =
+        encodeBase(src, DIGITS_LOWER)
+    fun encodeUpper(src: ByteArray): String =
+        encodeBase(src, DIGITS_UPPER)
+
+    private fun encodeBase(data: ByteArray, digits: String = DIGITS): String {
+        val out = StringBuilder(data.size * 2)
+        for (n in data.indices) {
+            val v = data[n].toInt() and 0xFF
+            out.append(digits[(v ushr 4) and 0xF])
+            out.append(digits[(v ushr 0) and 0xF])
+        }
+        return out.toString()
+    }
+}
+
+val ByteArray.hex: String get() = Hex.encodeLower(this)
+val String.hex: ByteArray get() = Hex.decode(this)
+
+typealias CancelHandler = Signal<Unit>
+
+class Signal<T>(val onRegister: () -> Unit = {}) { //: AsyncSequence<T> {
+    inner class Node(val once: Boolean, val item: (T) -> Unit) : Closeable {
+        override fun close() {
+            handlers.remove(this)
+        }
+    }
+
+    private var handlersToRun = ArrayList<Node>()
+    private var handlers = ArrayList<Node>()
+    private var handlersNoOnce = ArrayList<Node>()
+
+    val listenerCount: Int get() = handlers.size
+
+    fun once(handler: (T) -> Unit): Closeable = _add(true, handler)
+    fun add(handler: (T) -> Unit): Closeable = _add(false, handler)
+
+    fun clear() = handlers.clear()
+
+    private fun _add(once: Boolean, handler: (T) -> Unit): Closeable {
+        onRegister()
+        val node = Node(once, handler)
+        handlers.add(node)
+        return node
+    }
+
+    operator fun invoke(value: T) {
+        val oldHandlers = handlers
+        handlersNoOnce.clear()
+        handlersToRun.clear()
+        for (handler in oldHandlers) {
+            handlersToRun.add(handler)
+            if (!handler.once) handlersNoOnce.add(handler)
+        }
+        val temp = handlers
+        handlers = handlersNoOnce
+        handlersNoOnce = temp
+
+        for (handler in handlersToRun) {
+            handler.item(value)
+        }
+    }
+
+    operator fun invoke(handler: (T) -> Unit): Closeable = add(handler)
+}
+
+interface Consumer<T> : Closeable {
+    suspend fun consume(cancel: CancelHandler? = null): T?
+}
+
+interface Producer<T> : Closeable {
+    fun produce(v: T): Unit
+}
+
+open class ProduceConsumer<T> : Consumer<T>, Producer<T> {
+    private val items = LinkedList<T?>()
+    private val consumers = LinkedList<(T?) -> Unit>()
+    private var closed = false
+
+    val availableCount get() = synchronized(this) { items.size }
+
+    override fun produce(v: T) {
+        synchronized(this) { items.addLast(v) }
+        flush()
+    }
+
+    override fun close() {
+        synchronized(this) {
+            items.addLast(null)
+            closed = true
+        }
+        flush()
+    }
+
+    private fun flush() {
+        while (true) {
+            var done = false
+            var consumer: ((T?) -> Unit)? = null
+            var item: T? = null
+            synchronized(this) {
+                if (consumers.isNotEmpty() && items.isNotEmpty()) {
+                    consumer = consumers.removeFirst()
+                    item = items.removeFirst()
+                } else {
+                    done = true
+                }
+            }
+            if (done) break
+            consumer!!(item)
+        }
+    }
+
+    override suspend fun consume(cancel: CancelHandler?): T? = suspendCoroutine { c ->
+        val consumer: (T?) -> Unit = {
+            c.resume(it)
+            //if (it != null) c.resume(it) else c.resumeWithException(EOFException())
+        }
+        if (cancel != null) {
+            cancel {
+                synchronized(this) {
+                    consumers -= consumer
+                }
+                c.resumeWithException(CancellationException(""))
+            }
+        }
+        synchronized(this) {
+            consumers += consumer
+        }
+        flush()
+    }
+}
+
+/*
+fun String.toByteArray(): ByteArray {
+    val out = ByteArray(this.length)
+    for (n in 0 until out.size) out[n] = this[n].toByte()
+    return out
+}
+*/
