f56a0588ab1151de5a0eeaace2d5f4bd41531845
Carlos Ballesteros Velasco <soywiz@gmail.com>
2018-03-12 07:16:37 +0100

    Proper postgre test

--- ktor-client-cio-postgre/src/test/kotlin/com/soywiz/io/ktor/client/postgre/PostgreTest.kt
+++ ktor-client-cio-postgre/src/test/kotlin/com/soywiz/io/ktor/client/postgre/PostgreTest.kt
@@ -3,16 +3,20 @@ package com.soywiz.io.ktor.client.postgre
 import kotlinx.coroutines.experimental.*
 import kotlinx.coroutines.experimental.io.*
 import org.junit.*
+import kotlin.test.*
 
 class PostgreTest {
     @Test
-    fun name() {
+    fun testWritePostgreStartup() {
         runBlocking {
             val bc = ByteChannel(true)
             bc.writePostgreStartup("hello")
             val packet = bc._readPostgrePacket(readType = false)
-            println(packet.payload.toString(Charsets.ISO_8859_1))
-            println(packet.payload.toList())
+
+            assertEquals(
+                "\u0000\u0003\u0000\u0000user\u0000hello\u0000database\u0000hello\u0000application_name\u0000ktor-cio\u0000client_encoding\u0000UTF8\u0000\u0000",
+                packet.payload.toString(Charsets.UTF_8)
+            )
         }
     }
 }
