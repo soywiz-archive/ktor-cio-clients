571eefb7f277f8a512774f4548f4ce2083fe218b
Carlos Ballesteros Velasco <soywiz@gmail.com>
2018-03-12 05:16:09 +0100

    Postgre some package transfer work

--- ktor-client-cio-postgre/src/main/kotlin/com/soywiz/io/ktor/client/postgre/Postgre.kt
+++ ktor-client-cio-postgre/src/main/kotlin/com/soywiz/io/ktor/client/postgre/Postgre.kt
@@ -11,25 +11,40 @@ import kotlinx.coroutines.experimental.io.*
 class PostgrePacket(val type: Int, val payload: ByteArray) {
 }
 
-private suspend fun ByteReadChannel.readPostgrePacket(): PostgrePacket {
+internal suspend fun ByteReadChannel.readPostgrePacket(): PostgrePacket {
+    return _readPostgrePacket(readType = true)
+}
+
+internal suspend fun ByteReadChannel._readPostgrePacket(readType: Boolean): PostgrePacket {
     readByteOrder = ByteOrder.LITTLE_ENDIAN
-    val type = readByte().toInt() and 0xFF
+    val type = if (readType) readByte().toInt() and 0xFF else 0
     val size = readInt()
     return PostgrePacket(type, readBytesExact(size - 4))
 }
 
-private suspend fun ByteWriteChannel.writePostgreStartup() {
+internal suspend fun ByteWriteChannel.writePostgreStartup(user: String, database: String? = null, vararg params: Pair<String, String>) {
     val packet = PostgrePacket(0, buildByteArray {
-        writeInt(0x0003_0000) // The protocol version number. The most significant 16 bits are the major version number (3 for the protocol described here). The least significant 16 bits are the minor version number (0 for the protocol described here).
-
+        write32_le(0x0003_0000) // The protocol version number. The most significant 16 bits are the major version number (3 for the protocol described here). The least significant 16 bits are the minor version number (0 for the protocol described here).
+        val pairArray = arrayListOf<Pair<String, String>>().apply {
+            add("user" to user)
+            if (database != null) add("database" to database)
+            addAll(params)
+        }
+        for ((key, value) in pairArray) {
+            writeStringz(key)
+            writeStringz(value)
+        }
+        writeStringz("")
     })
     _writePostgrePacket(packet, first = true)
 }
 
 private suspend fun ByteWriteChannel._writePostgrePacket(packet: PostgrePacket, first: Boolean) {
+    writeByteOrder = ByteOrder.LITTLE_ENDIAN
     if (!first) writeByte(packet.type)
     writeInt(4 + packet.payload.size)
     writeFully(packet.payload)
+    flush()
 }
 
-private suspend fun ByteWriteChannel.writePostgrePacket(packet: PostgrePacket) = _writePostgrePacket(packet, first = false)
+internal suspend fun ByteWriteChannel.writePostgrePacket(packet: PostgrePacket) = _writePostgrePacket(packet, first = false)
--- ktor-client-cio-postgre/src/test/kotlin/com/soywiz/io/ktor/client/postgre/PostgreTest.kt
+++ ktor-client-cio-postgre/src/test/kotlin/com/soywiz/io/ktor/client/postgre/PostgreTest.kt
@@ -1,2 +1,18 @@
 package com.soywiz.io.ktor.client.postgre
 
+import kotlinx.coroutines.experimental.*
+import kotlinx.coroutines.experimental.io.*
+import org.junit.*
+
+class PostgreTest {
+    @Test
+    fun name() {
+        runBlocking {
+            val bc = ByteChannel(true)
+            bc.writePostgreStartup("hello")
+            val packet = bc._readPostgrePacket(readType = false)
+            println(packet.payload.toString(Charsets.ISO_8859_1))
+            println(packet.payload.toList())
+        }
+    }
+}
