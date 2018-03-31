7794c8056f2eaa852ef6ecea5fc8022f062e4b35
soywiz <soywiz@gmail.com>
2018-03-31 10:29:23 +0200

    Fixed test and checks that it works with and without useByteReadReadInt

--- ktor-client-cio-postgre/src/test/kotlin/com/soywiz/io/ktor/client/postgre/PostgreTest.kt
+++ ktor-client-cio-postgre/src/test/kotlin/com/soywiz/io/ktor/client/postgre/PostgreTest.kt
@@ -9,14 +9,20 @@ class PostgreTest {
     @Test
     fun testWritePostgreStartup() {
         runBlocking {
-            val bc = ByteChannel(true)
-            bc.writePostgreStartup("hello")
-            val packet = bc._readPostgrePacket(readType = false)
+            for (useByteReadReadInt in listOf(true, false)) {
+                val bc = ByteChannel(true)
+                bc.writePostgreStartup("hello")
+                val packet = bc._readPostgrePacket(
+                    readType = false, config = PostgresConfig(
+                        useByteReadReadInt = useByteReadReadInt
+                    )
+                )
 
-            assertEquals(
-                "\u0000\u0003\u0000\u0000user\u0000hello\u0000database\u0000hello\u0000application_name\u0000ktor-cio\u0000client_encoding\u0000UTF8\u0000\u0000",
-                packet.payload.toString(Charsets.UTF_8)
-            )
+                assertEquals(
+                    "\u0000\u0003\u0000\u0000user\u0000hello\u0000database\u0000hello\u0000application_name\u0000ktor-cio\u0000client_encoding\u0000UTF8\u0000\u0000",
+                    packet.payload.toString(Charsets.UTF_8)
+                )
+            }
         }
     }
 }
