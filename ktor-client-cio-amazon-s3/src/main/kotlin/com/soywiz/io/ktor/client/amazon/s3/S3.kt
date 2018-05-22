a3e6812c706756bcd9840875d16e4d95af5f7c96
Carlos Ballesteros Velasco <soywiz@gmail.com>
2018-05-22 02:09:52 +0200

    Ported S3 to ktor

--- ktor-client-cio-amazon-s3/src/main/kotlin/com/soywiz/io/ktor/client/amazon/s3/S3.kt
+++ ktor-client-cio-amazon-s3/src/main/kotlin/com/soywiz/io/ktor/client/amazon/s3/S3.kt
@@ -1,20 +1,51 @@
 package com.soywiz.io.ktor.client.amazon.s3
 
-/*
-// http://docs.amazonwebservices.com/AmazonS3/latest/dev/RESTAuthentication.html#ConstructingTheAuthenticationHeader
-class S3(val credentials: AmazonAuth.Credentials?, val endpoint: String, val httpClient: HttpClient, val timeProvider: TimeProvider) {
-    override fun getAbsolutePath(path: String) = parsePath(path).absolutePath
-    override val supportedAttributeTypes = listOf(ACL::class, MimeType::class)
+import com.soywiz.io.ktor.client.amazon.auth.*
+import com.soywiz.io.ktor.client.util.*
+import io.ktor.client.*
+import io.ktor.client.call.*
+import io.ktor.client.engine.apache.*
+import io.ktor.client.request.*
+import io.ktor.client.response.*
+import io.ktor.content.*
+import io.ktor.http.*
+import kotlinx.coroutines.experimental.io.*
+import java.nio.charset.*
 
+// http://docs.amazonwebservices.com/AmazonS3/latest/dev/RESTAuthentication.html#ConstructingTheAuthenticationHeader
+// https://github.com/jubos/fake-s3
+class S3(
+    val credentials: AmazonAuth.Credentials?,
+    val endpointPattern: String,
+    val httpClient: HttpClient,
+    val timeProvider: () -> Long
+) {
     companion object {
-        suspend operator fun invoke(region: String = Environment.get("AWS_DEFAULT_REGION") ?: "eu-west-1", accessKey: String? = null, secretKey: String? = null, httpClient: HttpClient = HttpClient(), timeProvider: TimeProvider = TimeProvider()): S3 {
-            return S3(AmazonAuth.getCredentials(accessKey, secretKey), "s3-$region.amazonaws.com", httpClient, timeProvider)
+        suspend operator fun invoke(
+            endpoint: String = "https://{bucket}.s3-{region}.amazonaws.com/{key}",
+            region: String = System.getenv("AWS_DEFAULT_REGION") ?: "eu-west-1",
+            accessKey: String? = null,
+            secretKey: String? = null,
+            httpClient: HttpClient = HttpClient(Apache),
+            timeProvider: () -> Long = { System.currentTimeMillis() }
+        ): S3 {
+            return S3(
+                AmazonAuth.getCredentials(accessKey, secretKey),
+                endpoint.replace("{region}", region),
+                httpClient,
+                timeProvider
+            )
         }
+    }
+
+    data class ParsedPath(val bucket: String, val key: String, val endpointPattern: String) {
+        val url = endpointPattern.replace("{bucket}", bucket).replace("{key}", key)
+        val cannonical = "/$bucket/$key"
     }
 
     private fun normalizePath(path: String) = "/" + path.trim('/')
 
-    class ACL(var text: String) : Vfs.Attribute {
+    class ACL(var text: String) {
         companion object {
             val PRIVATE = ACL("private")
             val PUBLIC_READ = ACL("public-read")
@@ -27,71 +58,101 @@ class S3(val credentials: AmazonAuth.Credentials?, val endpoint: String, val htt
         }
     }
 
-    suspend override fun stat(path: String): VfsStat {
-        val result = request(Http.Method.HEAD, path)
+    suspend fun stat(path: String): Stat {
+        val result = request(HttpMethod.Head, path)
 
-        return if (result.success) {
-            createExistsStat(path, isDirectory = true, size = result.headers.getFirst("content-length")?.toLongOrNull() ?: 0L)
+        return if (result.status.isSuccess()) {
+            Stat(exists = true, length = Dynamic { result.headers["content-length"].long })
         } else {
-            createNonExistsStat(path)
+            Stat(exists = false)
         }
     }
 
-    suspend override fun open(path: String, mode: VfsOpenMode): AsyncStream {
-        if (mode.write) invalidOp("Use put for writing")
-        return request(Http.Method.GET, path).content.toAsyncStream()
+    data class Stat(val exists: Boolean, val length: Long = 0L)
+
+    suspend fun get(
+        path: String,
+        range: LongRange? = null
+    ): ByteReadChannel {
+        return request(HttpMethod.Get, path, Headers.build {
+            if (range != null) {
+                set("Range", "bytes=${range.start}-${range.endInclusive}")
+            }
+        }).content
     }
 
-    suspend override fun put(path: String, content: AsyncInputStream, attributes: List<Attribute>): Long {
-        if (content !is AsyncStream) invalidOp("S3.put requires AsyncStream")
-        val access = attributes.get<ACL>() ?: ACL.PRIVATE
-        val contentType = attributes.get<MimeType>() ?: PathInfo(path).mimeTypeByExtension
-        //val contentLength = content.getAvailable()
-
+    suspend fun put(
+        path: String,
+        content: OutgoingContent,
+        access: ACL = ACL.PRIVATE,
+        contentType: ContentType = ContentType.defaultForFilePath(path)
+    ): Long {
+        val length = content.contentLength ?: error("contentLength must be set for content")
         request(
-            Http.Method.PUT,
+            HttpMethod.Put,
             path,
-            contentType = contentType.mime,
-            headers = Http.Headers(
-                "content-type" to contentType.mime,
-                "content-length" to "${content.getLength()}", // @TODO: @KOTLIN @BUG error: java.lang.VerifyError: Bad type on operand stack
-                //"content-length" to "$contentLength",
-                "x-amz-acl" to access.text
-            ),
+            headers = Headers.build {
+                contentType(contentType)
+                contentLength(length)
+                set("x-amz-acl", access.text)
+            },
             content = content
         )
 
-        return content.getLength()
+        return length
     }
 
-    suspend fun request(method: Http.Method, path: String, contentType: String = "", contentMd5: String = "", headers: Http.Headers = Http.Headers(), content: AsyncStream? = null): HttpClient.Response {
+    suspend fun request(
+        method: HttpMethod,
+        path: String,
+        headers: Headers = Headers.build { },
+        content: OutgoingContent? = null
+    ): HttpResponse {
         val npath = parsePath(path)
-        return httpClient.request(
-            method, npath.absolutePath,
-            headers = genHeaders(method, npath, headers.withReplaceHeaders(
-                "date" to AmazonAuth.V1.DATE_FORMAT.format(timeProvider.currentTimeMillis())
-            )),
-            content = content
+        val mheaders = genHeaders(
+            method, npath, headers.withReplaceHeaders(
+                "date" to AmazonAuth.V1.DATE_FORMAT.format(timeProvider())
+            )
         )
-    }
-
-    data class ParsedPath(val bucket: String, val key: String) {
-        val absolutePath = "https://$bucket.s3.amazonaws.com/$key"
-        val cannonical = "/$bucket/$key"
+        return httpClient.call {
+            this.method = method
+            println(npath.url)
+            this.url(npath.url)
+            this.headers { appendAll(mheaders) }
+            if (content != null) {
+                this.body = content
+            }
+        }.response
     }
 
     private fun parsePath(path: String): ParsedPath {
         val npath = path.trim('/')
         val parts = npath.split('/', limit = 2)
-        return ParsedPath(parts[0].trim('/'), parts.getOrElse(1) { "" }.trim('/'))
+        return ParsedPath(parts[0].trim('/'), parts.getOrElse(1) { "" }.trim('/'), endpointPattern)
     }
 
-    suspend private fun genHeaders(method: Http.Method, path: ParsedPath, headers: Http.Headers = Http.Headers()): Http.Headers {
-        if (credentials != null) {
-            return headers.withReplaceHeaders("Authorization" to AmazonAuth.V1.getAuthorization(credentials.accessKey, credentials.secretKey, method, path.cannonical, headers))
-        } else {
-            return headers
-        }
+    private suspend fun genHeaders(
+        method: HttpMethod,
+        path: ParsedPath,
+        headers: Headers = Headers.build { }
+    ): Headers = if (credentials != null) {
+        headers.withReplaceHeaders(
+            "Authorization" to AmazonAuth.V1.getAuthorization(
+                credentials.accessKey,
+                credentials.secretKey,
+                method,
+                path.cannonical,
+                headers
+            )
+        )
+    } else {
+        headers
     }
 }
-*/
+
+private fun Headers.withReplaceHeaders(vararg items: Pair<String, String>): Headers {
+    return Headers.build {
+        appendAll(this@withReplaceHeaders)
+        for ((key, value) in items) set(key, value)
+    }
+}
---
+++ ktor-client-cio-amazon-s3/src/test/kotlin/com/soywiz/io/ktor/client/amazon/s3/S3Spike.kt
@@ -0,0 +1,32 @@
+package com.soywiz.io.ktor.client.amazon.s3
+
+import io.ktor.content.*
+import kotlinx.coroutines.experimental.*
+import kotlinx.coroutines.experimental.io.*
+import java.nio.charset.*
+
+object S3Spike {
+    @JvmStatic
+    fun main(args: Array<String>) {
+        runBlocking {
+            val s3 = S3(
+                "http://127.0.0.1:4567/{bucket}/{key}",
+                accessKey = "demo", secretKey = "demo"
+            )
+            s3.put("mybucket/hello.txt", ByteArrayContentWithLength("HELLO WORLD"))
+            println(s3.stat("mybucket/hello.txt"))
+            println(s3.get("mybucket/hello.txt").readRemaining().readText())
+        }
+    }
+}
+
+// @TODO: ByteArrayContent should provide contentLength
+class ByteArrayContentWithLength(private val bytes: ByteArray) : OutgoingContent.ByteArrayContent() {
+    override val contentLength: Long = bytes.size.toLong()
+    override fun bytes(): ByteArray = bytes
+}
+
+fun ByteArrayContentWithLength(
+    str: String,
+    charset: Charset = Charsets.UTF_8
+): ByteArrayContentWithLength = ByteArrayContentWithLength(str.toByteArray(charset))
